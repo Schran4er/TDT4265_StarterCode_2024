task 2 complete, except for correct backprop or something

assignment2/task2.py
@@ -19,7 +19,11 @@ def calculate_accuracy(
         Accuracy (float)
     """
     # TODO: Implement this function (copy from last assignment)
-    accuracy = 0
+    predictions = model.forward(X)
+    predictions_rounded = (predictions >= 0.5).astype(int)
+    correct_predictions = (targets == predictions_rounded).sum() # "The prediction is determined as 1 if ŷ ≥ 0.5 else 0"
+
+    accuracy = correct_predictions / len(predictions)
     return accuracy
 
 
@@ -55,8 +59,11 @@ class SoftmaxTrainer(BaseTrainer):
 
         loss = 0
 
-            self.model.ws[layer_idx] = (
-                self.model.ws[layer_idx] - self.learning_rate * grad
+        logits = self.model.forward(X_batch)
+        self.model.backward(X_batch, logits, Y_batch)   
+
+        for layer_idx in range(len(self.model.ws)):
+            self.model.ws[layer_idx] = self.model.ws[layer_idx] - self.learning_rate * self.model.grads[layer_idx]
         loss=cross_entropy_loss(Y_batch, logits)  # sol
 
         return loss
@@ -109,6 +116,11 @@ def main():
     model=SoftmaxModel(
         neurons_per_layer, use_improved_sigmoid, use_improved_weight_init, use_relu
     )
+
+    for layer_idx, w in enumerate(model.ws):
+        model.ws[layer_idx] = np.random.uniform(-1, 1, size=w.shape)
+    # print(f"number of parameters (weights + biases): {model.ws[0].shape[0]*model.ws[0].shape[1] + model.ws[1].shape[0]*model.ws[1].shape[1]}")     # for 2d)
+
     trainer=SoftmaxTrainer(
         momentum_gamma,
         use_momentum,

assignment2/task2a.py
@@ -44,7 +44,8 @@ def cross_entropy_loss(targets: np.ndarray, outputs: np.ndarray, use_L2_reg=Fals
     assert targets.shape == outputs.shape,\
         f"Targets shape: {targets.shape}, outputs: {outputs.shape}"
 
-    C_n_w = -1*(np.sum(targets * np.log(outputs)))
+    # C_n_w = -1*(np.sum(targets * np.log(outputs)))
+    C_n_w = -1*(np.sum(targets * np.log(outputs + 1e-12)))      # add small number to avoid log(0) = -inf
     if use_L2_reg: C_n_w += l2_reg_lambda * np.sum(w**2)
     loss = C_n_w / len(targets)
     return loss
@@ -101,8 +102,9 @@ class SoftmaxModel:
         def sigmoid_derivative(x):
             return sigmoid(x) * (1 - sigmoid(x))
         def softmax(Z):
+            Z = Z - np.max(Z)       # Softmax numerical stability!
             return np.exp(Z) / np.sum(np.exp(Z))    # for input vector Z
-        
+
         # input -> hidden layer
         Z_J = X.dot(self.ws[0])
         A_J = sigmoid(Z_J)