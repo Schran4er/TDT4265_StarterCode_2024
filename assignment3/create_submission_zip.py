b9fa2610580f9d9b26c692c3475d90521e862dac
felix.rong <felix.rong2000@gmail.com>
2024-03-07 22:05:48 +0100

    export report and submission

--- assignment3/create_submission_zip.py
+++ assignment3/create_submission_zip.py
@@ -22,6 +22,9 @@ def query(question, options):
 files_to_include = {
     "task2": [".py"],
     "task2_train": [".ipynb"],
+    "task3": [".py"],
+    "task3_train": [".ipynb"],
+    "task4": [".py"],
     "trainer": [".py"],
     "utils": [".py"],
     "dataloaders": [".py"]
